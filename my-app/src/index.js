3a0c2bc6101fe12c1b778cd0aeace2c4a525807f
DreamSH-J <jangsen94@gmail.com>
2024-05-02 14:47:09 +0900

    React Hook

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import NotificationList from './Chapter6/NotificationList';
+import Accommodate from './Chapter7/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     // <React.StrictMode>
-      <NotificationList />
+      <Accommodate />
     // </React.StrictMode>
   );
 
